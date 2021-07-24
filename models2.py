6fabc895bc25f9f9f2ece3ea5da8f470d5fbb1a1
seeun329 <seeun329@gmail.com>
2021-07-24 21:38:58 +0900

    db 모델링 파일 추가

---
+++ models2.py
@@ -0,0 +1,20 @@
+from pybo import db
+
+class Question(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    round = db.Column(db.Integer,db.ForeignKey('round.id',ondelete='CASCADE'),primary_key=True)
+    content = db.Column(db.Text(),nullable=False)
+
+class Answer(db.Model):
+    question_id = db.Column(db.Integer,db.ForeignKey('question.id',ondelete='CASCADE'),primary_key=True)
+    question_round = db.Column(db.Integer,db.ForeignKey('question.round',ondelete='CASCADE'),primary_key=True)
+    content = db.Column(db.Text(),nullable=False)
+    score = db.Column(db.Float,nullable=False)
+    essay = db.Column(db.Boolean,nullable=False)
+    count = db.Column(db.Integer,nullable=False)
+    sum = db.Column(db.Integer,nullable=False)
+
+class Round(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    date = db.Column(db.DateTime(),nullable=False)
+    pass_rate = db.Column(db.Float)
