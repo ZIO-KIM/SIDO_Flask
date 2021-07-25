0391a6422195747b461e2429b6118e94c861d2d5
ZIO <zio0323gm@gmail.com>
2021-07-25 19:34:33 +0900

    models.py sido db 내용으로 수정

--- models.py
+++ models.py
@@ -2,13 +2,19 @@ from pybo import db
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(200), nullable=False)
+    round = db.Column(db.Integer, db.ForeignKey('round.id', ondelete='CASCADE'),primary_key=True)
     content = db.Column(db.Text(), nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False)
 
 class Answer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-    question = db.relationship('Question', backref=db.backref('answer_set'))
+    question_id = db.Column(db.Integer,db.ForeignKey('question.id',ondelete='CASCADE'),primary_key=True)
+    question_round = db.Column(db.Integer,db.ForeignKey('question.round',ondelete='CASCADE'),primary_key=True)
     content = db.Column(db.Text(), nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False)
+    score = db.Column(db.Float, nullable=False)
+    essay = db.Column(db.Boolean, nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+    sum = db.Column(db.Integer, nullable=False)
+
+class Round(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(), nullable=False)
+    pass_rate = db.Column(db.Float)
